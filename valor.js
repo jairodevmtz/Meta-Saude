c1ab9089836e64811056fc5607c511bbe3e68e10
FellipeRV <134394202+FellipeRV@users.noreply.github.com>
2023-06-12 17:55:48 -0300

    Alteração
    Correção de erros.

--- valor.js
+++ valor.js
@@ -94,6 +94,19 @@ function exibirConfirmacao(ofertaData) {
     document.body.appendChild(confirmacaoForm);
 }
 
+const confirmLink = document.getElementById('btval');
+if (confirmLink == null) {
+    alert('ta nulo');    
+}
+confirmLink.addEventListener('click', (event) => {
+    alert('Link clicado!');
+    event.preventDefault(); // Impede o comportamento padrão do link
+
+    // Atualize o link para "Oferta já feita"
+    confirmLink.textContent = 'Oferta já feita';
+    confirmLink.href = '#'; // Remova o link para impedir que o usuário clique novamente
+});
+
 function salvarOferta(ofertaData) {
     db.collection('pacientes').doc(ofertaData.pacienteId).get()
         .then((doc) => {
@@ -106,7 +119,13 @@ function salvarOferta(ofertaData) {
 
                 db.collection('ofertas').add(ofertaCompleta)
                     .then(() => {
+                        const felipet = document.getElementById('btval');
+                        felipet.textContent = 'Felipet'
+
                         alert('Oferta enviada com sucesso!');
+                        const confirmLink = document.getElementById('confirm-link');
+                        confirmLink.textContent = 'Oferta já feita';
+                        confirmLink.href = '#'; // Remova o link para impedir que o usuário clique novamente
                         window.location.href = 'hospital.html'; // Redireciona para a página principal após o envio
                     })
                     .catch((error) => {
@@ -120,3 +139,4 @@ function salvarOferta(ofertaData) {
             console.log('Erro ao buscar o paciente:', error);
         });
 }
+
